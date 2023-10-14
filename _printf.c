2eb4c6858d28966c5e21275bb0e11d5cef3ab3bd
Clear008 <wissal-exo@hotmail.com>
2023-10-14 13:55:57 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -3,22 +3,22 @@
 #include "main.h"
 
 /**
- * _printf - a function that prints anything
- * @format: list of types
- *
- * Return: the number of characters printed (excluding the null byte)
- */
+* _printf - a function that prints anything
+* @format: list of types
+*
+* Return: the number of characters printed (excluding the null byte)
+*/
 int _printf(const char *format, ...)
 {
-	int awinx = 0, awcount = 0;
-	va_list awls_rg;
+int awinx = 0, awcount = 0;
+va_list awls_rg;
 
-	if (format == NULL)
-	{
-		return (-1);
-	}
+if (format == NULL)
+{
+return (-1);
+}
 
-	va_start(awls_rg, format);
+va_start(awls_rg, format);
 while (format && format[awinx])
 {
 if (format[awinx] == '%')
@@ -29,6 +29,7 @@ else
 {
 awcount += aw_print_cha(format[awinx]);
 }
+
 awinx++;
 
 if (format[awinx] == '%')
@@ -39,7 +40,9 @@ if (!format[awinx])
 break;
 }
 }
+
 }
 va_end(awls_rg);
 return (awcount);
 }
+
