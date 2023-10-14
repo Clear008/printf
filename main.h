a238d3f010a7294f86297414079d0b4f1488f1bd
amessoudi <amessoudi150@gmail.com>
2023-10-14 11:58:05 +0100

    Added main.h

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+void aw_print_unsigned_integer(unsigned int n, int *count);
+void aw_print_integer(int n, int *count);
+int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
