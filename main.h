ee89330e615b9911398f1e947a41b09b3b9f3303
amessoudi <amessoudi150@gmail.com>
2023-10-15 14:18:12 +0100

    updated main.h for handling %b

--- main.h
+++ main.h
@@ -8,6 +8,7 @@ int aw_print_int(int n);
 int aw_print_cha(char c);
 int aw_print_str(const char *s);
 int aw_percent_sn(void);
+int aw_print_bin(unsigned int n);
 int _printf(const char *format, ...);
 int handle_format(const char *format, int *index, va_list args);
 
