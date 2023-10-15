59bd726b1d4ba045d34f5c03b91b5dc9977060a3
Clear008 <wissal-exo@hotmail.com>
2023-10-15 03:52:46 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -12,11 +12,10 @@ int _printf(const char *format, ...)
 {
 int awinx = 0, awcount = 0;
 va_list awls_rg;
-
-if (format == NULL)
-{
+if (!format || (format[0] == '%' && !format[1]))
 return (-1);
-}
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
 
 va_start(awls_rg, format);
 while (format && format[awinx])
