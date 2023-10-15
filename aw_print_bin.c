ab72e8927d269c65757a4552b7139257e0344768
amessoudi <amessoudi150@gmail.com>
2023-10-15 14:25:17 +0100

    Fixed Betty style comments

--- aw_print_bin.c
+++ aw_print_bin.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * aw_print_bin - Converts unsigned int to binary
+ * @aw_n: unsigned int
+ * Return: The number of d printed.
+ */
+
 int aw_print_bin(unsigned int aw_n)
 {
 	char aw_c;
@@ -12,5 +18,5 @@ int aw_print_bin(unsigned int aw_n)
 	aw_c = (aw_n % 2) + '0';
 	write(1, &aw_c, 1);
 
-	return aw_count + 1;
+	return (aw_count + 1);
 }
