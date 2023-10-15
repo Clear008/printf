0ee2d2c8523fe994dbffdf7ddcef7b466b5d9fc8
Clear008 <wissal-exo@hotmail.com>
2023-10-15 11:21:52 -0700

    betty style correction

--- aw_print_ups.c
+++ aw_print_ups.c
@@ -10,6 +10,7 @@
 int aw_print_ups(char *awst_r)
 {
 int awinx = 0;
+int awcount;
 char *aw_hex = "0123456789ABCDEF";
 char aw_hcode[4];
 if (awst_r == NULL)
@@ -27,12 +28,14 @@ aw_hcode[2] = aw_hex[(awst_r[awinx] / 16)];
 aw_hcode[3] = aw_hex[(awst_r[awinx] % 16)];
 
 write(1, aw_hcode, 4);
+awcount += 4;
 }
 else
 {
 write(1, &awst_r[awinx], 1);
+awcount++;
 }
 awinx++;
 }
-return (awinx);
+return (awcount);
 }
